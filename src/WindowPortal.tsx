2545d878df4b097b6ff05832e824f0294699034f
Agustín Ibars <agustin.ibars@gmail.com>
2020-12-22 12:08:57 -0300

    Use DesignerHost as children

--- src/WindowPortal.tsx
+++ src/WindowPortal.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Designer } from "@grapecity/activereports/reportdesigner";
 import styles from './DesignerHost.module.scss';
 import './DesignerHost.scss';
+import { DesignerHost } from './DesignerHost';
 
 
 interface IWindowPortalProps {
@@ -17,8 +18,6 @@ class WindowPortal extends React.PureComponent<IWindowPortalProps> {
     constructor(props: IWindowPortalProps) {
         super(props);
         this.containerEl = document.createElement('div'); // STEP 1: create an empty div
-        this.containerEl.id = 'designer-host';
-        this.containerEl.className = styles.designerHost;
         this.externalWindow = null;
     }
 
@@ -34,7 +33,6 @@ class WindowPortal extends React.PureComponent<IWindowPortalProps> {
                 targetDoc.head.appendChild(newStyleEl);
             } else if (styleSheet.href) { // true for stylesheets loaded from a URL
                 const newLinkEl = sourceDoc.createElement('link');
-
                 newLinkEl.rel = 'stylesheet';
                 newLinkEl.href = styleSheet.href;
                 targetDoc.head.appendChild(newLinkEl);
@@ -47,32 +45,10 @@ class WindowPortal extends React.PureComponent<IWindowPortalProps> {
         this.externalWindow.document.body.appendChild(this.containerEl);
 
         this.externalWindow.document.title = 'Report Designer window';
-        let s = document.createElement("script");
-        s.type = "text/javascript";
-        s.src = 'http://cdn.grapecity.com/activereportsjs/2.0.0/dist/ar-js-core.js';
-        this.externalWindow.document.head.append(s);
-
-        s = document.createElement("script");
-        s.type = "text/javascript";
-        s.src = 'http://cdn.grapecity.com/activereportsjs/2.0.0/dist/ar-js-designer.js';
-        this.externalWindow.document.head.append(s);
-
-
-
+      
         this.copyStyles(document, this.externalWindow.document);
         this.externalWindow.addEventListener('beforeunload', () => {
-
             this.props.closeWindowPortal();
-        });
-
-        s = document.createElement("script");
-        s.innerHTML = ' new GC.ActiveReports.ReportDesigner.Designer("#designer-host");';
-        this.externalWindow.document.body.appendChild(s);
-
-        this.externalWindow.addEventListener('load', (e) => {    
-            console.log(e); 
-            const designer = new Designer('#designer-host');
-            console.log(designer);
         });
     }
 
@@ -81,7 +57,7 @@ class WindowPortal extends React.PureComponent<IWindowPortalProps> {
     }
 
     render() {
-        return ReactDOM.createPortal(null, this.containerEl);
+        return ReactDOM.createPortal(<DesignerHost />, this.containerEl);
     }
 }
 
